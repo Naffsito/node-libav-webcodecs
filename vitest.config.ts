c540f62062507bd81b5dd7b3babfe5d1f971d4f9
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-11-30 23:00:52 +0100

    run in parallel

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     include: ['test/**/*.test.ts'],
     testTimeout: 30000,
     // Run sequentially in main thread
-    fileParallelism: false,
-    isolate: false,
+    // fileParallelism: false,
+    // isolate: false,
     pool: 'threads',
     // Setup file for browser polyfills (runs before all tests)
     setupFiles: ['./test/setup.ts'],
