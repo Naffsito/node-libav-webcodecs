7b324f43fb4e4a1c6b9b31036e4612c5b16455b4
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-11-30 21:27:26 +0100

    remove rollup. single threaded vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
     environment: 'node',
     include: ['test/**/*.test.ts'],
     testTimeout: 30000,
+    // Run sequentially in main thread
+    fileParallelism: false,
+    isolate: false,
   },
 });
